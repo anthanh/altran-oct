d54925cac6228919854e02bb8120d54285855566
Anthanh Pham Trinh <antai.pt@gmail.com>
2017-10-03 21:41:39 +0200

    javascript playground

---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+// alert('woho!');
+
+// Esta funciona hace una cosa super loca bla bla bla
+
+var number = 3;
+var cadena = 'esto es una cadena';
+var booleano = false;
+// var niIdea = undefined;
+var nose = null;
+var lista = [];
+var obj = {};
+// console.log(niIdea !== nose)
+
+console.log('log', 'other', 'other one', obj, number, lista);
+console.debug('debug');
+console.info('info');
+console.warn('warn');
+console.error('error');
+
+function myfunc() {
+  // TODO: we hace to refactor this code
+  return true;
+}
+
+/*
+lowerCamelCase
+UpperCamelCase
+kebab-case
+*/
+
+var listado = ['uno', 'dos', 'tres'];
+
+function mostrarEnLog(elemento) {
+  console.log(elemento);
+}
+
+listado.forEach(mostrarEnLog);
+
+
+// var mivar = null;
+// mivar = null;
