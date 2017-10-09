4c3a81a8439f221a37f2a9bbb93cfe9a3b23ec3c
Anthanh Pham Trinh <antai.pt@gmail.com>
2017-10-09 21:06:33 +0200

    mobile sidemenu example with javascript

--- scripts/nav.js
+++ scripts/nav.js
@@ -5,8 +5,14 @@
 (function() {
 
   $('#nav a').click(function(event) {
+    $('#nav.toggle').removeClass('toggle');
     $('#nav a.active').removeClass('active');
     $(event.target).addClass('active');
   });
 
-})()
+  $('#nav + button').click(function(event) {
+    // $(event.target).parent('nav').toggleClass('toggle');
+    $('#nav').toggleClass('toggle');
+  });
+
+})();
