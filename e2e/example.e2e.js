66ab644a7d4f7b8dde628279a197d09cf7919ac6
Anthanh Pham Trinh <antai.pt@gmail.com>
2017-10-23 22:40:37 +0200

    protractor as promised

--- e2e/example.e2e.js
+++ e2e/example.e2e.js
@@ -1,22 +1,31 @@
 browser.waitForAngularEnabled(false)
 
+waitForVisibleElement = (selector, timeout = 5000) => {
+  return browser.wait(
+    protractor.until.elementLocated(by.css(selector)),
+    timeout
+  )
+}
+
 describe('google page', () => {
   it('can submit a search', (done) => {
 
     browser.get('https://www.google.es');
 
-    element(by.css('#lst-ib')).sendKeys('example search');
-    element(by.css('#lst-ib')).sendKeys(protractor.Key.ENTER);
-    // element(by.css('input[jsaction="sf.chk"]')).click();
-
-    var text = element(by.css('#resultStats')).getText();
-    text.then((data) =>  {
+    waitForVisibleElement('#lst-ib').then(() => {
+      // document.querySelector('#lst-ib').value = 'example search'
+      return element(by.css('#lst-ib')).sendKeys('example search');
+    }).then(() => {
+      // document.querySelector('input[jsaction="sf.chk"]').click()
+      return element(by.css('#lst-ib')).sendKeys(protractor.Key.ENTER);
+    }).then(() => {
+      return waitForVisibleElement('#resultStats')
+    }).then(() => {
+      return element(by.css('#resultStats')).getText();
+    }).then((data) => {
       expect(data.indexOf('About') !== -1).toEqual(true);
       done();
     });
 
   });
 });
-
-// document.querySelector('#lst-ib').value = 'example search'
-// document.querySelector('input[jsaction="sf.chk"]').click()
